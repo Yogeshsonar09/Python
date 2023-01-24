3f5af94f335ddf500d4dd750eb5cafa1ab342a85
Yogesh Sonar <118359766+Yogeshsonar09@users.noreply.github.com>
2023-01-24 18:49:52 +0530

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,27 @@
+##Variable
+
+X=25
+# here x is variable and 25 is value
+
+print(X)
+
+##Example
+
+age=18
+print(age)
+
+country = ("United State")
+
+print(country)
+
+country=("Canada")
+
+
+print(country)
+
+#Assign One Variable to Other
+favorite_food=("steak")
+print(favorite_food)
+food =("pizza")
+favorite_food=food
+print(favorite_food)
