ed8a201662bb4a38d1e2debe1e7502f368643ca5
Yogesh Sonar <118359766+Yogeshsonar09@users.noreply.github.com>
2023-01-24 18:15:22 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,23 @@
+#function
+
+
+def m1():
+    print("m1----1111")##passing statement
+    print("m2---2222")
+
+m1()##function calling
+##example 2 basic function of addition and subtraction
+
+a=10
+b=20
+
+def add():
+    print(a+b)
+
+def sub():
+    print(b-a)
+add()
+sub()
+
+
+
